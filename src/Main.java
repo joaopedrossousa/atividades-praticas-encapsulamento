57a30038e1914989c2471e629f08020358dddc92
joaopedrossousa <jpsousa.ti@gmail.com>
2025-10-20 17:20:06 -0300

    Atividades Práticas Encapsulamento

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 import br.com.teste.atividadespraticas.ContaBancaria;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         ContaBancaria contaBancaria = new ContaBancaria();
+        Scanner entrada = new Scanner(System.in);
 
-        contaBancaria.setNumeroConta(123456789);
+        System.out.println("Informe o numero da Conta: ");
+        int dadosNumeroConta = entrada.nextInt();
+        contaBancaria.setNumeroConta(dadosNumeroConta);
         contaBancaria.setSaldo(2345.50);
         contaBancaria.setTitular("João Pedro Sousa");
 
         contaBancaria.exibirDadosConta();
 
-
+        entrada.close();
     }
 
 }
