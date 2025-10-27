193a51875ca42eafc157dc50b23da899f259c318
joaopedrossousa <jpsousa.ti@gmail.com>
2025-10-27 17:26:06 -0300

    Alterações Classe CC

--- src/br/com/atv/atividadespraticas/ContaBancaria.java
+++ src/br/com/atv/atividadespraticas/ContaBancaria.java
@@ -1,4 +1,34 @@
 package br.com.atv.atividadespraticas;
 
 public class ContaBancaria {
+    protected double saldo = 2000;
+
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+
+    }
+
+    public void consultarSaldo(){
+        System.out.println("Saldo Atual: R$%.2f".formatted(getSaldo()));
+    }
+
+    public void depositarSaldo(double valDepositado){
+        double saldoAtual = getSaldo();
+        saldoAtual += valDepositado;
+        setSaldo(saldoAtual);
+
+    }
+
+    public void sacarSaldo(double valSacado){
+        double saldoAtual = getSaldo();
+        if (valSacado <= saldoAtual){
+            saldoAtual -= valSacado;
+            setSaldo(saldoAtual);
+        }else{
+            System.out.println("Saldo insuficiente...");
+        }
+    }
 }
--- src/br/com/atv/atividadespraticas/ContaCorrente.java
+++ src/br/com/atv/atividadespraticas/ContaCorrente.java
@@ -1,4 +1,32 @@
 package br.com.atv.atividadespraticas;
 
-public class ContaCorrente {
+public class ContaCorrente extends ContaBancaria{
+    private double tarifaPorOperacao = 2;
+
+    public double getTarifa() {
+        return tarifaPorOperacao;
+    }
+
+    @Override
+    public void depositarSaldo(double valDepositado){
+        double saldoAtual = getSaldo();
+        super.depositarSaldo(valDepositado);
+        cobrarTarifa();
+
+    }
+    @Override
+    public void sacarSaldo(double valSacado) {
+        super.sacarSaldo(valSacado);
+        cobrarTarifa();
+
+    }
+
+
+    public void cobrarTarifa(){
+        System.out.println("Foi aplicada uma taxa de %.1f %% nessa transação...".formatted(getTarifa()));
+        double saldoAtual = getSaldo();
+        saldoAtual -= saldoAtual * (getTarifa() / 100);
+        setSaldo(saldoAtual);
+
+    }
 }
